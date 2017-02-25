59e1ebd5f4eab70e3f654db1fe6597a16af269de
Gmfholley <gmfholley@gmail.com>
2017-02-25 10:58:23 -0600

    Created one route for the about page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,29 +4,60 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 moduleForAcceptance('Acceptance | list rentals');
 
 test('should redirect to rentals route', function(assert) {
-  // visit('/list-rentals');
+	visit('/');
 
-  // andThen(function() {
-  //   assert.equal(currentURL(), '/list-rentals');
-  // });
-});
-
-test('should list available rentals', function(assert) {
+	andThen(function() {
+		assert.equal(currentURL(), '/rentals', 'should redirect to rentals');
+	});
 
 });
 
 test('should list available rentals', function(assert) {
+	visit('/');
 
+	andThen(function() {
+		assert.equal(find('.listing').length, 3, 'should be three listings');
+	});
 });
 
 test('should link to information about the company', function(assert) {
+	visit('/');
 
+	click('a:contains("About")');
+
+	andThen(function(){
+		assert.equal(currentURL(), '/about', 'about link should take you to about page');
+	})
+});
+
+test('should link to contact information', function(assert) {
+	visit('/');
+
+	click('a:contains("Contact")');
+
+	andThen(function(){
+		assert.equal(currentURL(), '/contact', 'contact link should take you to contact page');
+	});
 });
 
 test('should filter the list of rentals by city', function(assert) {
+	visit('/');
 
+	fillIn('.list-filter input', 'seattle');
+	keyEvent('.list-filter input', 'keyup', 69);
+
+	andThen(function(){
+		assert.equal(find('.listing').length, 1, 'should find one listing after filter');
+		assert.equal(find('.listing .location:contins("Seattle")').length, 1, 'should find that one listing contains word searched for');
+	})
 });
 
 test('should show details for a specific rental', function(assert) {
-
+	visit('/rentals');
+	click('a:contains("Grand Old Mansions")');
+	andThen(function(){
+		assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to grand old mansion page');
+		assert.equal(find('.show-listing h2').text(), "Grand Old Mansion", 'should show page with heading of rental listing');
+		assert.equal(find('.description').length, 1, 'should list a description of the rental listing');
+	})
 });
