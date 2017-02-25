b883edcc710524a5ef291d5caa413c7f62bee846
Gmfholley <gmfholley@gmail.com>
2017-02-25 10:23:31 -0600

    Created my first set of tests in ember app

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,32 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should redirect to rentals route', function(assert) {
+  // visit('/list-rentals');
+
+  // andThen(function() {
+  //   assert.equal(currentURL(), '/list-rentals');
+  // });
+});
+
+test('should list available rentals', function(assert) {
+
+});
+
+test('should list available rentals', function(assert) {
+
+});
+
+test('should link to information about the company', function(assert) {
+
+});
+
+test('should filter the list of rentals by city', function(assert) {
+
+});
+
+test('should show details for a specific rental', function(assert) {
+
+});
