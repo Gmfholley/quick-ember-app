2fa43760ae130ee43c4ff703f90feef4a993b2fc
Gmfholley <gmfholley@gmail.com>
2017-02-25 14:39:51 -0600

    Moves data from hard-coded route to mirage & model

---
+++ app/models/rental.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  owner: DS.attr(),
+  city: DS.attr(),
+  type: DS.attr(),
+  image: DS.attr(),
+  bedrooms: DS.attr(),
+  description: DS.attr()
+});
--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -2,33 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return[{
-			id: 'grand-old-mansion',
-			title: 'Grand Old Mansion',
-			owner: 'Veruca Salt',
-			city: 'San Francisco',
-			type: 'Estate',
-			bedrooms: 15,
-			image: 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.dynastyrentals.com%2Fwp-content%2Fimages%2Fmansion-rental2.jpg&imgrefurl=http%3A%2F%2Fwww.dynastyrentals.com%2Fmansions-for-rent%2F&docid=yVxw0Tri-K16wM&tbnid=L1kvBWG-g3bjlM%3A&vet=1&w=300&h=200&bih=756&biw=1597&q=rentals%20mansion&ved=0ahUKEwjN-6OT46vSAhVE7mMKHeE2AWIQMwgeKAIwAg&iact=mrc&uact=8',
-			description: 'This grand old mansion is a big house.'
-		},{
-			id: 'urban-living',
-			title: 'Urban Living',
-			owner: 'Wendy',
-			city: 'Seattle',
-			type: 'Condo',
-			bedrooms: 1,
-			image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwi5y_u746vSAhUk94MKHf39DT0QjRwIBw&url=http%3A%2F%2Fwww.perdidosuncondorental.com%2Ffeatured.html&bvm=bv.148073327,d.cGc&psig=AFQjCNHeLahHnqI9WlldLg7X2BLGnimBLA&ust=1488130284100687',
-			description: 'This adorable condo puts the city at your fingertips.'
-		},{
-			id: 'downtown-charm',
-			title: 'Downtown Charm',
-			owner: 'Serena',
-			city: 'Portland',
-			type: 'Apartment',
-			bedrooms: 3,
-			image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia1.popsugar-assets.com%2Ffiles%2Fthumbor%2Fdc7-j2F4EpqnB0xNJPwpR50ZXYE%2Ffit-in%2F1024x1024%2Ffilters%3Aformat_auto-!!-%3Astrip_icc-!!-%2F2016%2F12%2F27%2F852%2Fn%2F1922794%2Fcab689225862c0c105ef38.50878476_edit_img_cover_file_35462771_1471010520%2Fi%2FFirst-Apartment-Decorating-Ideas.jpg&imgrefurl=http%3A%2F%2Fwww.popsugar.com%2Fhome%2FFirst-Apartment-Decorating-Ideas-35462771&docid=u10F-nH_jf1DpM&tbnid=yN9bphrbtyQ_hM%3A&vet=1&w=960&h=1024&bih=756&biw=1597&q=apartment&ved=0ahUKEwjasovj46vSAhVBIWMKHasCB7UQMwhvKBMwEw&iact=mrc&uact=8',
-			description: 'Rent or buy in this charming apartment in downtown.'
-		}]
+		return this.get('store').findAll('rental');
 	}
 });
